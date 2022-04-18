efb98aa55b4fe79da377328e94b153b8d26dde07
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-04-18 17:56:13 +0300

    Convert page, mobile.

--- src/pages/CurrencyConverter.jsx
+++ src/pages/CurrencyConverter.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import './CurrencyConverter.css';
+
 function CurrencyConverter() {
   return (
-    <div>
-      CurrencyConverter Page
+    <div className="converter">
+      <form>
+        <div className="output">200 RUB</div>
+        <label htmlFor="currency">
+          Enter amount
+        </label>
+        <input type="text" name="currency" id="currency" />
+        <input type="submit" value="Convert" />
+      </form>
     </div>
   );
 }
