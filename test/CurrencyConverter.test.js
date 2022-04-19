0a5dac075ef8f98617a40812451d89b928a5c933
Ilyas Garipov <ilyasgaripov97@gmail.com>
2022-04-19 11:31:48 +0300

    Remove redundant tests.

--- test/CurrencyConverter.test.js
+++ test/CurrencyConverter.test.js
@@ -1,6 +1,4 @@
 import {
-  extractBaseCurrency,
-  extractTargetCurrency,
   parseInput,
   convert,
 } from '../src/api/converter';
@@ -12,24 +10,6 @@ test('15 usd in rub', () => {
     amount: 15,
     target: 'RUB',
   });
-});
-
-test("extract base currency from '15 usd in rub' ", () => {
-  const s = '15 usd in rub'
-  const currencies = { usd: 12.5, rub: 9.99, bam: 0.03 };
-  expect(extractBaseCurrency(s, currencies)).toBe('USD');
-});
-
-test("extract base currency with no currencies", () => {
-  const s = "15 abcdefgh";
-  const currencies = { usd: 12.5, rub: 9.99, bam: 0.03 };
-  expect(extractBaseCurrency(s, currencies)).toBe("");
-})
-
-test("extract target currency from '15 usd in rub'", () => {
-  const s = '15 usd in rub'
-  const currencies = { usd: 12.5, rub: 9.99, bam: 0.03 };
-  expect(extractTargetCurrency(s, currencies)).toBe('RUB');
 });
 
 test("convert 15 usd to rub", () => {
